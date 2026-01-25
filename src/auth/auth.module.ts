4f1a2021fc957555cced31585a06663c692d52e8
aldnnnz <sayabukanrobot999@gmail.com>
2026-01-25 13:36:43 +0700

    refactor: dotenv to @nestjs/config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,14 +2,19 @@ import { Module } from '@nestjs/common'
 import { JwtModule } from '@nestjs/jwt'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: {
-        expiresIn: '7d',
-      },
+    JwtModule.registerAsync({
+      imports : [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '7d',
+        },
+      }),
     }),
   ],
   controllers: [AuthController],
