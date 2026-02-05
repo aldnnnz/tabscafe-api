55fa2a737c0d872623e111fedd737feb0982c8e2
aldnnnz <sayabukanrobot999@gmail.com>
2026-02-05 12:11:01 +0700

    import AuthModule

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [ProductController],
   providers: [ProductService],
 })
